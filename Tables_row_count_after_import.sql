c68cbd90c7e9d79f24dbf6dfbdd02d68c5f6d492
iragomez68 <iragomez.lj@gmail.com>
2019-07-26 12:05:13 -0400

    Changes to the ERD, tables row count script and the notebook

--- Tables_row_count_after_import.sql
+++ Tables_row_count_after_import.sql
@@ -1,14 +1,14 @@
 --Check row count for each table
-select 'employees' as tablename, count(*) from employees
+select 'employees' as tablename, count(*) as rowcount, 2 as orderby from employees
 union
-select 'departments' as tablename, count(*) from departments
+select 'departments' as tablename, count(*) as rowcount, 1 as orderby from departments
 union
-select 'dept_emp' as tablename, count(*) from dept_emp
+select 'dept_emp' as tablename, count(*) as rowcount, 4 as orderby  from dept_emp
 union
-select 'dept_manager' as tablename, count(*) from dept_manager
+select 'dept_manager' as tablename, count(*)  as rowcount, 3 as orderby  from dept_manager
 union 
-select 'salaries' as tablename, count(*) from salaries
+select 'salaries' as tablename, count(*)  as rowcount, 6 as orderby from salaries
 union
-select 'titles' as tablename, count(*) from titles
-order by tablename;
+select 'titles' as tablename, count(*) as rowcount, 7 as orderby  from titles
+order by orderby;
 
